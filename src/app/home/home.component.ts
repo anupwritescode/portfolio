81aa39d6640111c29bcefd87dc06c8a6707a9559
Anup Salve <anupsalve8@gmail.com>
2024-07-29 16:16:45 +0530

    add contact section and navbar, remove snake game

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,7 +16,7 @@ export class HomeComponent implements OnInit {
 `Hi there! 
 I'm Anup, a web developer
 from Pune, India. 
-Welcome to my site`;
+Welcome to my site.`;
 
   DELAY_CONST: number = 50;
   grid: number[][] = [];
@@ -28,7 +28,6 @@ Welcome to my site`;
 
   ngOnInit(): void {
     this.updateText();
-    this.fillGrid();
   }
 
   createObs(someText: string) {
@@ -44,58 +43,7 @@ Welcome to my site`;
     this.createObs(this.textContent).subscribe({
       next: (t) => {
         this.text += t;
-      },
-      complete: () => {
-        this.addDots();
-      },
-    });
-  }
-
-  addDots() {
-    this.createObs("...").subscribe({
-      next: (t) => {
-        this.dots += t;
-      },
-      complete: () => {
-        this.addSpaces();
-      },
-    });
-  }
-
-  whereToAddR = 13;
-  whereToAddC = 0;
-  added = 0;
-  addSpaces() {
-    const moveDots = this.createObs("                                         ");
-    moveDots.subscribe({
-      next: (t) => {
-        this.spaces += t;
-        const h1 = document.querySelector("div h1");
-        const pre = document.querySelector("div h1 pre");
-        if(h1 && pre) {
-          const isOverFlowing = pre?.clientWidth > h1?.clientWidth;
-          if(isOverFlowing) this.startSnakeGame = true;
-          if(isOverFlowing && this.added < 3) {
-            this.grid[this.whereToAddR][this.whereToAddC++] = 1;
-            this.added++;
-          }
-          if(this.added > 3) {
-            // start snake game
-          }
-        }
-      },
-      complete: () => {
       }
     });
   }
-
-  fillGrid() {
-    for(let i=0 ; i<20; i++) {
-      this.grid.push([]);
-      for(let j = 0; j < 20; j++) {
-        this.grid[i].push(0);
-      }
-    }
-  }
-
 }
